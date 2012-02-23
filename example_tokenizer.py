4632fba88292d1975577053447b1d85e4527619b
Isaac <hyperneato@gmail.com>
2012-02-23 01:02:00 -0500

    An example of a simple parser for the text of emails. This does not parse text from emails; rather, it parses a list of strings passed to __init__(). It y

--- example_tokenizer.py
+++ example_tokenizer.py
@@ -2,7 +2,7 @@ import generic_token as tkn
 import re
 
 class SpamTokenizer(object):
-    """parse email content, given as a list of strings broken by whitespace"""
+    """parse email content, given as a list of strings- a list of lines in a file."""
     def __init__(self, items):
         """Accepts: a list of email 'body' lines.
         Returns: a list of tokens"""
@@ -10,7 +10,9 @@ class SpamTokenizer(object):
         self.tokens = []
 
     def parse_pre_tokens(self, symbol):
-        """remove '=', '><', and any word that does not contain letters"""
+        """remove '=', '><', and any word that does not contain letters; or
+        if the word 'spam' occurs, remove that word from the token list. 
+        remove cruft from html."""
         if ('=' in symbol) or ('><' in symbol):
             return None
         spam_in_subject = re.compile('spam', re.I)
